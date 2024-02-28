7be7d55eb0e6c64818baade87fa9fe6ee959707b
Владислав Волков <v.volkov@rit.va>
2024-02-28 14:28:18 +0300

    add selectors and redux searchValue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    axios
+      .get(
+        'https://catalog.api.2gis.com/3.0/items?q=минск соборы&page=3&page_size=10&offset=20&fields=items.external_content,items.description,items.schedule,items.comment,items.reviews,items.rubrics,items.flags,items.delivery,rating&key=54a7d73b-0b3f-409c-88c5-11639e5f16f8',
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  }, []);
+
+  return <div className="Axx"></div>;
 }
 
 export default App;
