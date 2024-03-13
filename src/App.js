c46d0903cf600d2b5f2412e27e07c54d4d7941cc
Владислав Волков <v.volkov@rit.va>
2024-03-13 12:48:23 +0300

    add project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
+import React from 'react';
+import Home from './pages/Home.tsx';
+import './scss/components/app.scss';
 
-function App() {
-  useEffect(() => {
-    axios
-      .get(
-        'https://catalog.api.2gis.com/3.0/items?q=минск соборы&page=3&page_size=10&offset=20&fields=items.external_content,items.description,items.schedule,items.comment,items.reviews,items.rubrics,items.flags,items.delivery,rating&key=54a7d73b-0b3f-409c-88c5-11639e5f16f8',
-      )
-      .then((res) => {
-        console.log(res);
-      });
-  }, []);
-
-  return <div className="Axx"></div>;
-}
+export const App = () => {
+  return <Home />;
+};
 
 export default App;
